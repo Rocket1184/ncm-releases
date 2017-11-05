0c493c5ca2407ec05173e78d31d9b2b20eaf444c
rocka <i@rocka.me>
2017-11-05 22:01:46 +0800

    experimental: show commit messages as change log

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const qs = require('querystring');
+
 const pug = require('pug');
 const koa = require('koa');
 const route = require('koa-route');
+const serve = require('koa-static');
 const qiniu = require('qiniu');
+const fetch = require('node-fetch');
 
 const mac = new qiniu.auth.digest.Mac(process.env.QINIU_ACCESS_KEY, process.env.QINIU_SECRET_KEY);
 const config = new qiniu.conf.Config();
@@ -21,9 +25,9 @@ function formatSize(val) {
     return `${val.toFixed(1)} YB`;
 }
 
-function formatDate(timespan) {
+function formatDate(timestamp) {
     const to2Digit = num => num < 10 ? `0${num}` : num;
-    const dt = new Date(timespan / 10000);
+    const dt = new Date(timestamp / 10000);
     const M = parseInt(dt.getMonth()) + 1;
     const D = dt.getDate();
     const h = dt.getHours();
@@ -31,22 +35,59 @@ function formatDate(timespan) {
     return `${to2Digit(M)}-${to2Digit(D)} ${to2Digit(h)}:${to2Digit(m)}`;
 }
 
-function getFiles(suffix = '') {
+function getCommits(since, until = new Date().toISOString()) {
+    let url = `https://api.github.com/repos/Rocket1184/electron-netease-cloud-music/commits`;
+    let query = {};
+    if (since) query.since = since;
+    if (until) query.until = until;
+    url += `?${qs.stringify(query)}`;
+    return fetch(url).then(r => r.json());
+}
+
+async function addChangeLog(files) {
+    await Promise.all(files.map(async (value, index) => {
+        let previous = files[index + 1];
+        if (previous) {
+            value.commits = await getCommits(files[index + 1].timestamp, value.timestamp);
+        } else {
+            value.commits = await getCommits('', value.timestamp);
+        }
+    }));
+}
+
+function getFiles() {
     const options = {
         limit: 20,
         prefix: '',
     };
     return new Promise((res, rej) => {
-        bucketManager.listPrefix(`electron-netease-cloud-music${suffix}`, options, (err, respBody, respInfo) => {
+        bucketManager.listPrefix(`electron-netease-cloud-music`, options, (err, respBody, respInfo) => {
             if (!err && respInfo.statusCode === 200) {
                 respBody.items.sort((a, b) => b.putTime - a.putTime);
-                const list = respBody.items.map(i => ({
-                    name: i.key.replace('electron-ncm-', ''),
-                    size: formatSize(i.fsize),
-                    time: formatDate(i.putTime),
-                    url: `http://ncm${suffix}.rocka.cn/${i.key}`
-                }));
-                res(list);
+                const result = [];
+                respBody.items.forEach(item => {
+                    const hashReg = /-([a-z0-9]{7}).tar.gz/g;
+                    const typeReg = /([a-z0-9]+\-[a-z0-9]+)/g;
+                    const parsed = {
+                        name: item.key.replace('electron-ncm-', ''),
+                        hash: hashReg.exec(item.key)[1] || 'unknown_hash',
+                        size: formatSize(item.fsize),
+                        time: formatDate(item.putTime),
+                        url: `http://ncm.rocka.cn/${item.key}`
+                    };
+                    const type = typeReg.exec(parsed.name)[1] || 'Unknown OS';
+                    const lastResult = result[result.length - 1] || {};
+                    if (lastResult.hash && lastResult.hash === parsed.hash) {
+                        lastResult.pkgs[type] = parsed;
+                    } else {
+                        result.push({
+                            hash: parsed.hash,
+                            timestamp: new Date(item.putTime / 10000).toISOString(),
+                            pkgs: { [type]: parsed }
+                        });
+                    }
+                });
+                res(result);
             } else {
                 rej(err);
             }
@@ -54,41 +95,55 @@ function getFiles(suffix = '') {
     });
 }
 
-let binaryData = {
-    data: {
-        Info: [
-            {
-                name: 'Please refresh page and try again...',
-                size: '∑(￣□￣;)',
-                time: formatDate(Date.now()),
-                url: 'javascript:window.location.reload()'
+let avaliableBuilds = {
+    data: [
+        {
+            hash: 'unknown_hash',
+            timestamp: new Date().toISOString(),
+            commits: [
+                {
+                    sha: 'xxxxxxx',
+                    commit: {
+                        message: 'what\'s this?'
+                    }
+                }
+            ],
+            pkgs: {
+                'Unknown OS': {
+                    name: 'not_avaliable',
+                    hash: 'unknown_hash',
+                    size: -1,
+                    time: formatDate(Date.now()),
+                    url: 'javascript:location.reload();'
+                }
             }
-        ]
-    }
+        }
+    ]
 };
 
-async function refreshBinaryData() {
+async function refreshAvaliableBuilds() {
     try {
-        // const [master, dev] = await Promise.all([getFiles(), getFiles('-dev')]);
-        // binaryData = { data: { master, dev } };
-        const [master] = await Promise.all([getFiles()]);
-        binaryData = { data: { master } };
+        const files = await getFiles();
+        await addChangeLog(files);
+        avaliableBuilds = { data: files };
     } catch (err) {
-        console.log(`Error when refreshBinaryData: ${JSON.stringify(err, null, 4)}`);
-        return refreshBinaryData();
+        console.log('Error when refreshBinaryData:', err);
+        return refreshAvaliableBuilds();
     }
 };
 
-refreshBinaryData();
-setInterval(refreshBinaryData, 8 * 1000);
+refreshAvaliableBuilds();
+setInterval(refreshAvaliableBuilds, 8 * 1000);
 
 async function indexHandler(ctx, next) {
-    ctx.body = renderIndex(binaryData);
+    ctx.body = renderIndex(avaliableBuilds);
 }
 
 const app = new koa();
 
 app.use(route.get('/', indexHandler));
+
+app.use(serve('./static'));
 
 const port = process.env.PORT || 11233;
 app.listen(port, () => {
