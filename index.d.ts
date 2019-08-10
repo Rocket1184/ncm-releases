59ca5288c7d740570bc611f06030b3eb0f91ff78
Rocka <i@rocka.me>
2019-08-10 14:54:19 +0800

    refactor for new name schema

---
+++ index.d.ts
@@ -0,0 +1,119 @@
+// from `@octokit/rest`
+type ReposListCommitsResponseItemParentsItem = { url: string; sha: string };
+type ReposListCommitsResponseItemCommitter = {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+};
+type ReposListCommitsResponseItemAuthor = {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+};
+type ReposListCommitsResponseItemCommitVerification = {
+    verified: boolean;
+    reason: string;
+    signature: null;
+    payload: null;
+};
+type ReposListCommitsResponseItemCommitTree = { url: string; sha: string };
+type ReposListCommitsResponseItemCommitCommitter = {
+    name: string;
+    email: string;
+    date: string;
+};
+type ReposListCommitsResponseItemCommitAuthor = {
+    name: string;
+    email: string;
+    date: string;
+};
+type ReposListCommitsResponseItemCommit = {
+    url: string;
+    author: ReposListCommitsResponseItemCommitAuthor;
+    committer: ReposListCommitsResponseItemCommitCommitter;
+    message: string;
+    tree: ReposListCommitsResponseItemCommitTree;
+    comment_count: number;
+    verification: ReposListCommitsResponseItemCommitVerification;
+};
+type ReposListCommitsResponseItem = {
+    url: string;
+    sha: string;
+    node_id: string;
+    html_url: string;
+    comments_url: string;
+    commit: ReposListCommitsResponseItemCommit;
+    author: ReposListCommitsResponseItemAuthor;
+    committer: ReposListCommitsResponseItemCommitter;
+    parents: Array<ReposListCommitsResponseItemParentsItem>;
+};
+
+namespace Gh {
+    type ReposListCommitsResponse = Array<ReposListCommitsResponseItem>;
+}
+
+namespace Qn {
+    interface File {
+        key: string;
+        hash: string;
+        fsize: number;
+        mimeType: string;
+        putTime: number;
+        type: number;
+        status: number;
+    }
+}
+
+namespace Ncm {
+    interface Pkg {
+        name: string;
+        size: string;
+        url: string;
+    }
+    interface Commit {
+        sha?: string;
+        commit: {
+            message: string;
+        }
+    }
+    interface Build {
+        hash: string;
+        timestamp: number;
+        commits?: Commit[];
+        pkgs: {
+            [key: string]: Pkg
+        };
+    }
+}
+
+expors as namespace;
--- index.js
+++ index.js
@@ -1,26 +1,20 @@
+// @ts-check
 'use strict';
-
-const qs = require('querystring');
 
 const pug = require('pug');
 const koa = require('koa');
 const route = require('koa-route');
 const serve = require('koa-static');
-const qiniu = require('qiniu');
-const fetch = require('node-fetch');
-
 const _ = {
-    groupBy: require('lodash.groupby')
+    groupBy: require('lodash/groupBy')
 };
 
-const mac = new qiniu.auth.digest.Mac(process.env.QINIU_ACCESS_KEY, process.env.QINIU_SECRET_KEY);
-const config = new qiniu.conf.Config();
-config.useHttpsDomain = true;
-config.zone = qiniu.zone.Zone_z0;
-const bucketManager = new qiniu.rs.BucketManager(mac, config);
+const { getCommits } = require('./lib/github');
+const { listBucketFiles } = require('./lib/qiniu');
 
-const renderIndex = pug.compileFile('./static/index.pug');
-
+/**
+ * @param {number} val
+ */
 function formatSize(val) {
     for (const unit of ['', 'K', 'M']) {
         if (val < 1000) return `${val.toFixed(1)} ${unit}B`;
@@ -29,27 +23,9 @@ function formatSize(val) {
     return `${val.toFixed(1)} GB`;
 }
 
-function formatDate(timestamp) {
-    const dt = new Date(timestamp / 10000);
-    return dt.toLocaleDateString('zh', {
-        hour12: false,
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit'
-    });
-}
-
-function getCommits(since, until = new Date().toISOString()) {
-    return fetch(`https://api.github.com/repos/Rocket1184/electron-netease-cloud-music/commits?${qs.stringify({ since, until })}`, {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: `token ${process.env.GITHUB_TOKEN}`
-        }
-    }).then(response => response.json());
-}
-
+/**
+ * @param {Ncm.Build[]} releases
+ */
 async function addChangeLog(releases) {
     await Promise.all(releases.map(async (value, index) => {
         if (index + 1 < releases.length) {
@@ -57,7 +33,7 @@ async function addChangeLog(releases) {
             value.commits = await getCommits(releases[index + 1].timestamp, value.timestamp);
         } else {
             // it's the oldest build
-            value.commits = await getCommits(new Date(0).toISOString(), value.timestamp);
+            value.commits = await getCommits(0, value.timestamp);
             if (value.commits.length > 9) {
                 value.commits = value.commits.slice(0, 9);
                 value.commits.push({ commit: { message: 'and more ...' } });
@@ -66,71 +42,71 @@ async function addChangeLog(releases) {
     }));
 }
 
-function listPrefix(prefix = 'electron-netease-cloud-music') {
-    const options = {
-        limit: 1024,
-        prefix: '',
-    };
-    return new Promise((resolve, reject) => {
-        bucketManager.listPrefix(prefix, options, (err, respBody, respInfo) => {
-            if (!err && respInfo.statusCode === 200) {
-                resolve(respBody.items);
-            } else {
-                reject(err);
-            }
-        });
-    });
-}
-
-function groupReleasesByCommit(items) {
+/**
+ * @param {Qn.File[]} items
+ * @returns {{[key: string]: Qn.File[]}}
+ */
+function groupFilesByVersion(items) {
     return _.groupBy(items, item => {
-        const regxp = /[-_]([a-z0-9]{7})\./;
-        const hash = regxp.exec(item.key);
-        return hash[1] || 'unknown';
+        const regxp = /_(v\w+\.\w+\.\w+-\d+-g[0-9a-f]+)/;
+        const version = regxp.exec(item.key)[1];
+        return version || 'unknown';
     });
 }
 
-function groupPackageByPlatform(pkgs) {
-    const result = {};
-    for (const pkg of pkgs) {
+/**
+ * @param {Qn.File[]} files
+ * @returns {{[key: string]: Ncm.Pkg}}
+ */
+function groupPackageByPlatform(files) {
+    let result = {};
+    for (const file of files) {
+        /** @type {Ncm.Pkg} */
         const parsed = {
-            size: formatSize(pkg.fsize),
-            time: formatDate(pkg.putTime),
-            url: `http://ncm.qn.rocka.cn/${pkg.key}`
+            name: file.key,
+            size: formatSize(file.fsize),
+            url: `http://ncm.qn.rocka.cn/${file.key}`
         };
-        if (pkg.key.endsWith('.asar')) {
+        if (file.key.endsWith('.asar')) {
             result['asar'] = parsed;
-        } else if (pkg.key.endsWith('.tar.gz')) {
-            if (pkg.key.includes('linux-x64')) {
-                result['linux-x64'] = parsed;
-            } else if (pkg.key.includes('darwin-x64')) {
-                result['darwin-x64'] = parsed;
+        } else if (file.key.endsWith('.tar.gz')) {
+            if (file.key.includes('linux')) {
+                result['linux'] = parsed;
+            } else if (file.key.includes('darwin')) {
+                result['darwin'] = parsed;
             }
         }
     }
+    // @ts-ignore
     return result;
 }
 
+/**
+ * @returns {Promise<Ncm.Build[]>}
+ */
 async function getFiles() {
     const result = [];
-    const versions = groupReleasesByCommit(await listPrefix());
-    for (const hash in versions) {
+    const files = await listBucketFiles();
+    const groups = groupFilesByVersion(files);
+    for (const [version, files] of Object.entries(groups)) {
         result.push({
-            hash,
-            timestamp: versions[hash][0].putTime,
-            pkgs: groupPackageByPlatform(versions[hash])
+            hash: version,
+            timestamp: Math.trunc(files[0].putTime / 10000),
+            pkgs: groupPackageByPlatform(files)
         });
     }
     result.sort((a, b) => b.timestamp - a.timestamp);
-    result.forEach(r => r.timestamp = new Date(r.timestamp / 10000).toISOString());
     return result;
 }
 
+/**
+ * @type {{data: Ncm.Build[]}}
+ */
 let avaliableBuilds = {
     data: [
         {
             hash: 'Load failed',
-            timestamp: new Date().toISOString(),
+            timestamp: Date.now(),
             commits: [
                 {
                     sha: 'xxxxxxx',
@@ -142,9 +118,7 @@ let avaliableBuilds = {
             pkgs: {
                 'Refresh': {
                     name: 'not_avaliable',
-                    hash: 'unknown_hash',
-                    size: -1,
-                    time: formatDate(Date.now()),
+                    size: 'not_avaliable',
                     url: 'javascript:location.reload();'
                 }
             }
@@ -152,29 +126,32 @@ let avaliableBuilds = {
     ]
 };
 
+/**
+ * @param {Ncm.Build[]} input
+ */
 function countPkg(input) {
     return input.reduce((prev, cur) => Object.keys(cur.pkgs).length + prev, 0);
 }
 
 async function refreshAvaliableBuilds() {
     try {
-        const files = await getFiles();
-        const cnt = countPkg(files);
+        const releases = await getFiles();
+        const cnt = countPkg(releases);
         // due to GitHub API [rate-limiting](https://developer.github.com/v3/#rate-limiting)
         // we can only request 60 times per hour.
         // so do not refresh change log when avaliable builds have no changes
         if (cnt === countPkg(avaliableBuilds.data)) return;
         console.log(`countPkg(getFiles()); // ${cnt}`);
-        await addChangeLog(files);
+        await addChangeLog(releases);
         console.log('addChangeLog();');
-        avaliableBuilds = { data: files };
+        avaliableBuilds = { data: releases };
     } catch (e) {
         console.error(`refreshAvaliableBuilds(); // ${e}`);
         return refreshAvaliableBuilds();
     }
 }
 
-(async function entryPoint() {
+(async function loop() {
     // eslint-disable-next-line no-constant-condition
     while (true) {
         await refreshAvaliableBuilds();
@@ -182,13 +159,14 @@ async function refreshAvaliableBuilds() {
     }
 })();
 
-async function indexHandler(ctx) {
-    ctx.body = renderIndex(avaliableBuilds);
-}
-
 const app = new koa();
 
-app.use(route.get('/', indexHandler));
+app.use(route.get('/', ctx => {
+    if (process.env.NODE_ENV !== 'development' && ctx.protocol === 'http') {
+        return ctx.redirect(ctx.url.replace(/^http/, 'https'));
+    }
+    ctx.body = pug.renderFile('./static/index.pug', avaliableBuilds)
+}));
 
 app.use(serve('./static'));
 
